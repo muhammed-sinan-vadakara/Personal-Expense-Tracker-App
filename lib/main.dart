b2df3f2afd7a6dd6d2f5609f4666b6067de42f41
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-05-17 22:07:05 +0530

    light and dark theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
+import 'package:expense_tracker_app/core/themes/theme_provider.dart';
 import 'package:expense_tracker_app/features/authentication/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Expense Tracker App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ref.watch(themeProvider),
       home: const LoginPage(),
     );
   }
