d8d2414cbb35faefa24a6b3158a9c724e69af3fa
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-05-17 21:43:53 +0530

    first commit

---
+++ lib/features/authentication/presentation/pages/login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+///Stateless widget
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
